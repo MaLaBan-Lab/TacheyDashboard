e0ce16ef6d63b31b4dec03baa11aaf5c867241e0
WIP_WU <0>
2021-07-08 02:24:59 +0800

    基本版面

---
+++ TacheyDashboard/Controllers/OrdersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TacheyDashboard.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public IActionResult Point()
+        {
+            return View();
+        }
+
+        public IActionResult Invite()
+        {
+            return View();
+        }
+
+        public IActionResult Order()
+        {
+            return View();
+        }
+    }
+}
