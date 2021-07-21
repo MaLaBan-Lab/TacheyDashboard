bdac046fcd190d0f9c13c1d58e2c496ef01cb21a
Emily <b10620002@g.chu.edu.tw>
2021-07-21 17:13:46 +0800

    .

--- TacheyDashboard/Controllers/OrdersController.cs
+++ TacheyDashboard/Controllers/OrdersController.cs
@@ -3,26 +3,42 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TacheyDashboard.Models;
+using TacheyDashboard.Service;
+using Newtonsoft.Json;
 
 namespace TacheyDashboard.Controllers
 {
     public class OrdersController : Controller
     {
-        public OrdersController()
+        private readonly TacheyContext _context;
+        private readonly OrdersService _ordersService;
+        public OrdersController(TacheyContext context)
         {
+            _context = context;
+            _ordersService = new OrdersService(context);
         }
         public IActionResult Point()
         {
+            var result = _ordersService.getpointmodel();
+            var jsonString = JsonConvert.SerializeObject(result);
+            ViewBag.jsonString=jsonString;
             return View();
         }
 
         public IActionResult Invite()
         {
+            var result = _ordersService.GetTickets();
+            var jsonString = JsonConvert.SerializeObject(result);
+            ViewBag.jsonString = JsonConvert.SerializeObject(result);
             return View();
         }
 
         public IActionResult Order()
         {
+            var result=_ordersService.GetOrderViewModels();
+            var jsonString = JsonConvert.SerializeObject(result);
+            ViewBag.jsonString = JsonConvert.SerializeObject(result);
             return View();
         }
     }
--- TacheyDashboard/Service/OrdersService.cs
+++ TacheyDashboard/Service/OrdersService.cs
@@ -2,10 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Tachey001.Repository;
+using TacheyDashboard.Models;
+using TacheyDashboard.ViewModel;
 
 namespace TacheyDashboard.Service
 {
     public class OrdersService
     {
+        private readonly TacheyContext _context;
+        public OrdersService(TacheyContext context) //自動幫我new
+        {
+            _context = context;
+        }
+
+        public List<OrderViewModel> GetOrderViewModels()
+        {
+            var order = _context.Orders;
+
+            var result = (from o in order
+
+                          select new OrderViewModel
+                          {
+                              OrderId = o.OrderId,
+                              MemberId = o.MemberId,
+                              PayMethod = o.PayMethod,
+                              OrderDate = o.OrderDate,
+                              OrderStatus = o.OrderStatus,
+                              //TotalPrice = GetTotalPrice(o.OrderId)
+                          }).ToList(); //tolist才能給result
+
+            foreach (var item in result)
+            {
+                item.TotalPrice = GetTotalPrice(item.OrderId);
+            }
+            return result;
+        }
+        //傳OrderID進來比對資料
+        public decimal GetTotalPrice(string OrderId)
+        {
+            var orderdetail = _context.OrderDetails;
+            decimal result = 0;
+            foreach (var item in orderdetail)
+            {
+                if (item.OrderId == OrderId)
+                {
+                    result = (decimal)(result + item.UnitPrice);
+                }
+            }
+
+            return result;
+        }
+        public List<Point> getpointmodel()
+        {
+            var point = _context.Points;
+            var result = point.ToList();
+            return result;
+        }
+        public List<Ticket> GetTickets()
+        {
+            var ticket = _context.Tickets;
+            var result = ticket.ToList();
+            return result;
+        }
     }
 }
