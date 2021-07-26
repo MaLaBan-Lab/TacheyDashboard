8ef1f46ff3e789c6fb876ff04b1a261c2162fd96
WIP_WU <0>
2021-07-27 00:43:08 +0800

    CourseCategory update

--- TacheyDashboard/Controllers/CoursesController.cs
+++ TacheyDashboard/Controllers/CoursesController.cs
@@ -1,18 +1,29 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TacheyDashboard.Models;
+using TacheyDashboard.Service;
 
 namespace TacheyDashboard.Controllers
 {
     [Authorize]
     public class CoursesController : Controller
     {
-        public CoursesController()
+        private readonly TacheyContext _context;
+
+        private CoursesService _coursesService;
+
+        public CoursesController(CoursesService coursesService)
         {
+            _context = new TacheyContext();
+
+            _coursesService = coursesService;
         }
+
         public IActionResult Product()
         {
             return View();
@@ -20,6 +31,29 @@ namespace TacheyDashboard.Controllers
 
         public IActionResult Application()
         {
+            return View();
+        }
+
+        public IActionResult CourseCategory()
+        {
+            var resultData = _coursesService.GetAllCourseCategory();
+
+            string JsonResultData = JsonConvert.SerializeObject(resultData);
+
+            ViewBag.Labels = JsonResultData;
+
+            var resultDataDetail = _coursesService.GetAllCategoryDetail();
+
+            string JsonResultDataDetail = JsonConvert.SerializeObject(resultDataDetail);
+
+            ViewBag.LabelsDetail = JsonResultDataDetail;
+
+            var resultId = _coursesService.GetLastCourseCategoryId();
+
+            string JsonResultId = JsonConvert.SerializeObject(resultId);
+
+            ViewBag.LastLabelsId = JsonResultId;
+
             return View();
         }
     }
--- TacheyDashboard/Service/CoursesService.cs
+++ TacheyDashboard/Service/CoursesService.cs
@@ -2,10 +2,108 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Tachey001.Repository;
+using TacheyDashboard.Models;
+using TacheyDashboard.ViewModel.Courses;
 
 namespace TacheyDashboard.Service
 {
     public class CoursesService
     {
+        private TacheyRepository _tacheyRepository;
+        public CoursesService(TacheyRepository tacheyRepository)
+        {
+            _tacheyRepository = tacheyRepository;
+        }
+
+        public List<CoursesViewModel> GetAllCourseCategory()
+        {
+            var category = _tacheyRepository.GetAll<CourseCategory>();
+
+            var detail = _tacheyRepository.GetAll<CategoryDetail>();
+
+            //var result = from c in category
+            //             join det in detail on c.CategoryId equals det.CategoryId
+            //             //group c by new { c.CategoryId, c.CategoryName } into g
+            //             select new CoursesViewModel
+            //             {
+            //                 CategoryId = c.CategoryId,
+            //                 CategoryName = c.CategoryName,
+            //                 DetailName = det.DetailName
+            //             };
+
+            //var result = from c in category
+            //             join det in detail on c.CategoryId equals det.CategoryId
+
+            //             group c by new { c.CategoryId, c.CategoryName } into g
+            //             select new
+            //             {
+            //                 CategoryId = g.Key.CategoryId,
+            //                 CategoryName = g.Key.CategoryName
+            //             };
+
+            //List<object> myLists = new List<object>();
+
+            //var result = categoryResult.GroupBy(x => x.CategoryId).Select(g => myLists.Add(g.Key));
+
+            var result = from c in category
+                         select new CoursesViewModel
+                         {
+                             CategoryId = c.CategoryId,
+                             CategoryName = c.CategoryName
+                         };
+
+            return result.ToList();
+        }
+
+        public List<CoursesViewModel> GetAllCategoryDetail()
+        {
+            var category = _tacheyRepository.GetAll<CourseCategory>();
+
+            var detail = _tacheyRepository.GetAll<CategoryDetail>();
+
+            var result = from d in detail
+                         select new CoursesViewModel
+                         {
+                             DetailName = d.DetailName
+                         };
+
+            return result.ToList();
+        }
+
+        public List<CoursesViewModel> GetAllCategoryDetail(int CategoryId)
+        {
+            var category = _tacheyRepository.GetAll<CourseCategory>();
+
+            var detail = _tacheyRepository.GetAll<CategoryDetail>();
+
+            var result = from d in detail
+                         where d.CategoryId == CategoryId
+                         select new CoursesViewModel
+                         {
+                             DetailName = d.DetailName
+                         };
+
+            return result.ToList();
+        }
+
+        public CoursesViewModel GetLastCourseCategoryId()
+        {
+            var category = _tacheyRepository.GetAll<CourseCategory>();
+
+            var detail = _tacheyRepository.GetAll<CategoryDetail>();
+
+            var categoryviewmodel = from c in category
+                         select new CoursesViewModel
+                         {
+                             CategoryId = c.CategoryId,
+                             CategoryName = c.CategoryName
+                         };
+
+
+            var result = categoryviewmodel.OrderByDescending(u => u.CategoryId).FirstOrDefault();
+
+            return result;
+        }
     }
 }
